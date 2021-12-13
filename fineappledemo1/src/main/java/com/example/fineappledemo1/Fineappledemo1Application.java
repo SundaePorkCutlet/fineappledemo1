487c7c5365bc4032137409f56b0018507eaa4a94
홍 <@DESKTOP-5AGTDL3>
2021-12-13 14:39:56 +0900

    g2

---
+++ fineappledemo1/src/main/java/com/example/fineappledemo1/Fineappledemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.fineappledemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fineappledemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fineappledemo1Application.class, args);
+	}
+
+}
